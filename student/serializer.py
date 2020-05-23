980e06a75cbcf7d21d65a32dc463f3be96b2463d
Mohan Krishna <mohankrishna.hb@hashedin.com>
2020-05-23 20:17:42 +0530

    creted studentserializer

---
+++ student/serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from student.models import Student
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['register_number', 'first_name',
+                  'last_name', 'email', 'phone_number']
