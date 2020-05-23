8390b648f55414cf343ba16f2a3dc25069149bad
Mohan Krishna <mohankrishna.hb@hashedin.com>
2020-05-23 19:58:08 +0530

    created model "student" Not made a migration file yet

--- student/models.py
+++ student/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    register_number = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    password = models.CharField(max_length=257)
+
+    def __str__(self):
+        return self.register_number
+
+    def name(self):
+        return self.first_name + ' ' + self.last_name
