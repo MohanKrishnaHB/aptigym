992ab72b95280e4ef646499d3ce5f76b67bfcc42
Mohan Krishna <mohankrishna.hb@hashedin.com>
2020-05-24 19:08:08 +0530

    created question and options models

--- question/admin.py
+++ question/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import CategoryLevel1, CategoryLevel2
+from .models import CategoryLevel1, CategoryLevel2, Question, Options
 
 admin.site.register(CategoryLevel1)
 admin.site.register(CategoryLevel2)
+admin.site.register(Question)
+admin.site.register(Options)
--- question/models.py
+++ question/models.py
@@ -11,18 +11,55 @@ class CategoryLevel1(models.Model):
 
 
 def create_default_category1():
-    default_categoory = CategoryLevel1.objects.filter(title="not assigned")
+    default_categoory = CategoryLevel1.objects.filter(title="other")
     if default_categoory.exists():
         return default_categoory[0]
     else:
-        return CategoryLevel1(title="not assigned").save()
+        return CategoryLevel1(title="other").save()
 
 
 class CategoryLevel2(models.Model):
     title = models.CharField(max_length=20, unique=True)
     category_level_1 = models.ForeignKey(
-        CategoryLevel1, default=0, on_delete=models.SET(create_default_category1))
+        CategoryLevel1, default=0, on_delete=models.SET(create_default_category1), related_name="sub_categories")
 
     def __str__(self):
         return self.title
 
+
+def create_default_category2():
+    default_categoory = CategoryLevel2.objects.filter(title="other")
+    if default_categoory.exists():
+        return default_categoory[0]
+    else:
+        return CategoryLevel2(title="other").save()
+
+
+class Question(models.Model):
+    question = models.TextField()
+    has_question_image = models.BooleanField(default=False)
+    question_image = models.ImageField(
+        upload_to='uploads/images/question/', null=True, blank=True)
+    solution = models.TextField()
+    has_solution_image = models.BooleanField(default=False)
+    solution_image = models.ImageField(
+        upload_to='uploads/images/solution/', null=True, blank=True)
+    category = models.ForeignKey(
+        CategoryLevel2, default=0, on_delete=models.SET(create_default_category2), related_name="questions")
+    created_on = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return "Question {}".format(self.id)
+
+
+class Options(models.Model):
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name='options')
+    option = models.CharField(max_length=100)
+    has_image = models.BooleanField(default=False)
+    image = models.ImageField(
+        upload_to='uploads/images/options/', null=True, blank=True)
+    is_true = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "question {} | option {}".format(self.question.id, self.id)
