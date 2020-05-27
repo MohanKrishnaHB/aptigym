72bb16399b16b1da37711588cb596bd016239131
Mohan Krishna <mohankrishna.hb@hashedin.com>
2020-05-27 16:16:04 +0530

    created institute model

--- mentor/models.py
+++ mentor/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from random import randint
 
-# Create your models here.
+
+def random_code():
+    d = randint(1000000, 100000000)
+    return hex(d)[2:8]
+
+
+class Institute(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    email = models.EmailField()
+    institute_code = models.CharField(
+        max_length=20, default=random_code, unique=True)
+    password = models.CharField(max_length=257)
